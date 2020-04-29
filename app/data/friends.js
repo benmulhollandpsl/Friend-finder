2c0ec55ad19f7eb1eb18332168c922b16b045e9c
Ben Mulholland <ben.mulholland@gmail.com>
2020-04-29 14:27:34 -0400

    initial commit of files and directories and friends.js next server and routes

---
+++ app/data/friends.js
@@ -0,0 +1,139 @@
+
+
+var friends = [
+    {
+      name: "Michaelangelo",
+      photo: "https://cdn3.volusion.com/ondfq.dtexo/v/vspfiles/photos/NE54054-4.jpg",
+      scores: [
+        "5",
+        "1",
+        "4",
+        "4",
+        "5",
+        "1",
+        "2",
+        "5",
+        "4",
+        "1"
+      ]
+    },
+    {
+      name: "Donatello",
+      photo: "https://i.ytimg.com/vi/UQvoCIzM6ug/maxresdefault.jpg",
+      scores: [
+        "4",
+        "2",
+        "5",
+        "1",
+        "3",
+        "2",
+        "2",
+        "1",
+        "3",
+        "2"
+      ]
+    },
+    {
+      name: "Leonardo",
+      photo: "https://i1.wp.com/www.teenagemutantninjaturtles.com/wp-content/uploads/2012/04/Leonardo-Teenage-Mutant-Ninja-Turtles-Movie-I.jpg,
+      scores: [
+        "5",
+        "2",
+        "2",
+        "2",
+        "4",
+        "1",
+        "3",
+        "2",
+        "5",
+        "5"
+      ]
+    },
+    {
+      name: "Raphael",
+      photo: "https://i.ebayimg.com/images/g/eR0AAOSwAitdFWUG/s-l300.jpg",
+      scores: [
+        "3",
+        "3",
+        "4",
+        "2",
+        "2",
+        "1",
+        "3",
+        "2",
+        "2",
+        "3"
+      ]
+    },
+    {
+      name: "Splinter",
+      photo: "https://vignette.wikia.nocookie.net/tmnt/images/d/df/2117065-sensi.jpg/revision/latest/scale-to-width-down/340?cb=20131102204058",
+      scores: [
+        "4",
+        "3",
+        "4",
+        "1",
+        "5",
+        "2",
+        "5",
+        "3",
+        "1",
+        "4"
+      ]
+    },
+    {
+        name: "Casey Jones",
+      photo: "https://vignette.wikia.nocookie.net/tmnt/images/1/16/Waynegretskionsteroids.png",
+      scores: [
+        "4",
+        "4",
+        "2",
+        "3",
+        "2",
+        "2",
+        "3",
+        "2",
+        "4",
+        "5"
+      ]
+    },
+    
+    {
+        name: "Krang",
+        photo: "https://img.cinemablend.com/filter:scale/cb/7/f/8/a/e/e/7f8aee6eb6c5f86d79cf77c776de59f0aba6b31b22066728c13b0110c8dbd947.jpg",
+        scores: [
+          "1",
+          "1",
+          "1",
+          "1",
+          "1",
+          "1",
+          "1",
+          "2",
+          "1",
+          "5"
+        ]
+      },
+
+      {
+        name: "April O'Neil",
+        photo: "https://vignette.wikia.nocookie.net/tmnt/images/0/04/April_1990.jpg",
+        scores: [
+          "5",
+          "5",
+          "5",
+          "5",
+          "5",
+          "5",
+          "5",
+          "5",
+          "5",
+          "5"
+        ]
+      }
+  
+  ];
+  
+  //export's array. accessible to files using require.
+  module.exports = friends;
+  
